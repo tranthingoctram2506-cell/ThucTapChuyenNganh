ee701776778567e4950d8a1504404948c2b4bc6b
DH52201601-TranThiNgocTram <tranthingoctram2506@gmail.com>
2025-11-20 23:06:20 +0700

    Thêm Authentication

--- project/resources/views/layout/admin.blade.php
+++ project/resources/views/layout/admin.blade.php
@@ -31,7 +31,7 @@
             </form>
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a href="#" class="btn btn-outline-light ms-2"><i class="fa fa-sign-out-alt"></i> Đăng xuất</a>
+                    <a class=" btn btn-outline-light ms-2" data-toggle="modal" data-target="#exampleModal" href="#" ><i class="fa fa-sign-out-alt"></i> Đăng xuất</a>
                 </li>
             </ul>
         </div>
@@ -60,12 +60,48 @@
         </main>
     </div>
 </div>
-
+<!-- Logout Modal-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a class="btn btn-primary" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+          </div>
+        </div>
+      </div>
+    </div>
 <footer class="bg-dark text-white p-3 mt-auto">
     <div class="container-fluid">
         ©Movie Review — Trang quản trị
     </div>
 </footer>
+
+<!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin.min.js"></script>
+    <!-- Custom scripts for this page-->
+    <script src="js/sb-admin-datatables.min.js"></script>
+    <script src="js/sb-admin-charts.min.js"></script>
 
 
 </body>
--- project/routes/web.php
+++ project/routes/web.php
@@ -1,29 +1,44 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth; 
 
 Route::get('/', function () {
     return view('index');
 })->name('home');
+
 Route::get('/admin', function () {
     return view('admin');
 })->name('admin');
-Route::get('/admin/quanlyphim', function () {
+
+Route::get('/quanlyphim', function () {
     return view('admin/quanlyphim/danhsachphim');
 })->name('danhsachphim');
-Route::get('/admin/theloai', function () {
+
+Route::get('/theloai', function () {
     return view('admin/quanlyphim/theloai');
 })->name('theloai');
-Route::get('/admin/phimchoduyet', function () {
+
+Route::get('/phimchoduyet', function () {
     return view('admin/quanlyreview/phimchoduyet');
 })->name('phimchoduyet');
-Route::get('/home/hanhdong', function () {
+
+Route::get('/hanhdong', function () {
     return view('home/theloai/hanhdong');
 })->name('hanhdong');
-Route::get('/home/kinhdi', function () {
+
+Route::get('/kinhdi', function () {
     return view('home/theloai/kinhdi');
 })->name('kinhdi');
-Route::get('/home/tintuc', function () {
+
+Route::get('/tintuc', function () {
     return view('home/tintuc');
 })->name('tintuc');
-?>
+
+Route::get('logout',[HomeController::class,'logout'
+])->name('logout');
+
+Auth::routes();
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
